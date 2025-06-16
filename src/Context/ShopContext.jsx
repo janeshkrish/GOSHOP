47c8b236a450d55f2396340f6b3f703f2e4b02a8
janesh <janeshkrishna12@gmail.com>
2025-06-16 10:32:04 +0530

    breadcrum added successfully

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,10 +1,10 @@
 import React, {createContext} from "react";
-import all_products from "../Components/Assets/all_product"; 
+import all_product from "../Components/Assets/all_product"; 
 
 export const ShopContext = createContext(null);
 
 const ShopContextProvider = (props) => {
-           const contextvalue ={all_products};
+           const contextvalue ={all_product};
 
            return(
             <ShopContext.Provider value={contextvalue}>
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../Context/ShopContext';
+import { useParams } from 'react-router-dom';
+import Breadcrum from '../Components/Breadcrums/Breadcrum'
+
 
 export const Product = () => {
+  const {all_product}= useContext(ShopContext);
+  const {productId} = useParams();
+  const product = all_product?.find((e) => e.id === Number(productId));
+  if (!product) {
+    console.warn('Product not found or not yet loaded.');
+    return null; // Don't render anything until product is ready
+  }
   return (
     <div>
-        
+        <Breadcrum product={product}/>
     </div>
   )
 }
